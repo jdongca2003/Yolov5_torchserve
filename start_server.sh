5feb907920590441c4591958052585fc8d4469ea
Dong, Jianxiong <jdongca2003@gmail.com>
2021-01-16 22:43:40 -0800

    update comments

--- start_server.sh
+++ start_server.sh
@@ -4,7 +4,7 @@ export JAVA_HOME=${CUR_DIR}/jdk-11
 export PATH=${JAVA_HOME}/bin:$PATH
 
 mkdir -p workspace
-#change tensorServer temp dir
+#change torchServe temp dir
 export TEMP=${CUR_DIR}/workspace
 
 torchserve --start --model-store model_store --models yolo5=yolo5.mar --ts-config ./config.properties
